510fc198beec1d0b983bd10764319d43976b66ce
Erik <86173146+erik-00@users.noreply.github.com>
2023-06-19 14:35:08 +0200

    Leben
    Lebensanzeige

---
+++ Assets/scripts/LivesGui2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesGui2 : MonoBehaviour {
+    [SerializeField] GameObject[] goodLives;
+    [SerializeField] GameObject[] badLives;
+    // Start is called before the first frame update
+    public void UpdateLives(int lives) {
+        for (int i = 0; i < goodLives.Length; i++) {
+            if (i < lives) {
+                goodLives[i].SetActive(true);
+                badLives[i].SetActive(false);
+            }
+            else {
+                goodLives[i].SetActive(false);
+                badLives[i].SetActive(true);
+            }
+        }
+    }
+
+    public void StartGame() {
+        for (int i = 0; i < goodLives.Length; i++) {
+            goodLives[i].SetActive(true);
+            badLives[i].SetActive(false);
+        }
+    }
+}
